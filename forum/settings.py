f75927245188d2e5d75c596d221aad9d67e1c5e6
hansenDise <todeeper@live.com>
2016-01-05 13:50:16 +0800

    alert website

--- forum/settings.py
+++ forum/settings.py
@@ -76,9 +76,17 @@ WSGI_APPLICATION = 'forum.wsgi.application'
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#    }
+    'default':{
+        'ENGINE':'django.db.backends.mysql',
+        'NAME':'popu',
+        'USER':'root',
+        'PASSWORD':'hansen',
+        'HOST':'127.0.0.1',
+        'PORT':3306,
     }
 }
 
--- forumsite/models.py
+++ forumsite/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+
+class Thread(models.Model):
+    threadid = models.AutoField(primary_key=True)
+    userid = models.IntegerField() 
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    posttime = models.DateTimeField()
+    commentcounts = models.IntegerField()
+    default_test = models.IntegerField(default=851)
+    
+    
+class Comment(models.Model):
+    commentid = models.AutoField(primary_key=True)
+    userid = models.IntegerField()
+    threadid = models.ForeignKey(Thread,db_column='threadid')
+    content = models.TextField()
+    commenttime = models.DateTimeField()
+    targetuserid = models.IntegerField()
+    isread = models.BooleanField()       
+    
