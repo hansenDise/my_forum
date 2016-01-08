83ba20dbd88d95d79aa83450be83c6ea66ee6b6d
hansenDise <todeeper@live.com>
2016-01-08 14:32:50 +0800

    alert login and resgiter part

--- forumsite/urls.py
+++ forumsite/urls.py
@@ -1,10 +1,12 @@
 from django.conf.urls import url
-
+from django.contrib.auth import views as auth_views
 import views
 
 urlpatterns = [
     url(r'^$',views.index),
     url(r'^login/$',views.login),
+    url(r'^logout/$',views.logout_view),
     url(r'^register/$',views.register),
     url(r'^threads/(?P<threadid>\w{7})$',views.thread),
+    url(r'^login_require/$',views.login_require),
 ]
--- forumsite/views.py
+++ forumsite/views.py
@@ -1,8 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import HttpResponse
+
+from django.contrib.auth import authenticate,logout,login as auth_login
+from django.contrib.auth.decorators import login_required
 # Create your views here.
 
 def index(request):
+    if not request.user.is_authenticated():
+        print 'not login'
+    else:
+        print 'already login'
     return render(request,'forumsite/forumIndex.html')
 
 
@@ -12,11 +19,36 @@ def thread(request,threadid):
 
 
 def login(request):
-    for key in request.POST:
-        print 'key = ',key
-    return HttpResponse("data posted success..")    
-    
+    print 'username =', request.POST['useraccount'] , ' password = ', request.POST['password']
+    user = authenticate(username=request.POST['useraccount'],password=request.POST['password'])
+    if user is not None:
+        if user.is_active:
+            auth_login(request,user)
+            return redirect('/')
+        else:
+            return HttpResponse("the password is valid , but the account has been disabled!")
+    else:
+        return HttpResponse("the username and password were incorrent.")
         
 
 def register(request):
-    pass
+    for key in request.POST:
+        print key , ' = ', request.POST[key]
+        
+    return HttpResponse("in register page.")
+
+
+def logout_view(request):
+    print '******* in logout*************'
+    if request.user.is_authenticated():
+        print 'authenticated'
+        logout(request)
+        return redirect('/')
+    else:
+        print 'not authen.'
+        return redirect('/')
+        
+
+@login_required(login_url='/login/')
+def login_require(request):
+    return HttpResponse("congrautuations , you have logined.")
