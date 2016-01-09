5ae21d65931f60a532590953632e9486d964eb08
hansenDise <todeeper@live.com>
2016-01-09 10:31:14 +0800

    pefect login and register

--- forumsite/views.py
+++ forumsite/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse
 
 from django.contrib.auth import authenticate,logout,login as auth_login
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
 # Create your views here.
 
 def index(request):
@@ -18,6 +19,11 @@ def login(request):
     if user is not None:
         if user.is_active:
             auth_login(request,user)
+            _temp_user = User.objects.get(username=request.POST['useraccount'])
+            if _temp_user is not None:
+                print '****** userid = ' ,_temp_user.id
+            else:
+                print '****** no username = ' ,request.POST['useraccount']
             return redirect('/')
         else:
             return HttpResponse("the password is valid , but the account has been disabled!")
@@ -26,17 +32,35 @@ def login(request):
         
 
 def register(request):
-    for key in request.POST:
-        print key , ' = ', request.POST[key]
+    username = request.POST['username']
+    email = request.POST['email']
+    password1 = request.POST['password1']
+    password2 = request.POST['password2']
+    
+    if password1 != password2:
+        return HttpResponse("password not the same , please retry!")
+    newuser = User.objects.create_user(username,email,password1)
+    if newuser is None:
+        return HttpResponse("register failed, please try agagin !")
+    else:
+        _tempUser = authenticate(username=username,password=password1)
         
-    return HttpResponse("in register page.")
+        print '****** , username:', _tempUser.username
+        
+        auth_login(request,_tempUser)
+        return redirect('/')
+    
+    
+    
 
 
 def logout_view(request):
     if request.user.is_authenticated():
         logout(request)
+    response = render(request,'forumsite/forumIndex.html')
+    response.delete_cookie('user')
     
-    return redirect('/')
+    return response
         
 
 @login_required(login_url='/login/')
